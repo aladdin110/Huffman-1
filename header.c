dcfc4d9cac4ea6b6c119070c6844d167b44564c9
tahirielm <76850118+tahirielm@users.noreply.github.com>
2021-11-10 23:03:12 +0100

    Update header.c

--- header.c
+++ header.c
@@ -21,3 +21,42 @@ void swapnode(struct node** a, struct node** b){
     *a = *b;
     *b = t;
 }
+
+void sort(struct huffman* huffman, int id){
+    int smallest = id;
+    int left = 2 * id + 1;
+    int right = 2 * id + 2;
+
+    if (left < huffman->size && huffman->array[left]->freq< huffman->array[smallest]->freq)
+        smallest = left;
+
+    if (right < huffman->size && huffman->array[right]->freq < huffman->array[smallest]->freq)
+        smallest = right;
+
+    if (smallest != id) {
+        swapnode(&huffman->array[smallest], &huffman->array[id]);
+        sort(huffman, smallest);
+    }
+}
+
+int isSizeOne(struct huffman* huffman){
+    return (huffman->size == 1);
+}
+
+struct node* extractMin(struct huffman* huffman){
+    struct node* temp = huffman->array[0];
+    huffman->array[0] = huffman->array[huffman->size - 1];
+    --huffman->size;
+    sort(huffman, 0);
+    return temp;
+}
+
+void inserthuffman(struct huffman* huffman,struct node* node){
+    ++huffman->size;
+    int i = huffman->size - 1;
+    while (i && node->freq < huffman->array[(i - 1) / 2]->freq) {
+        huffman->array[i] = huffman->array[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    huffman->array[i] = node;
+}
